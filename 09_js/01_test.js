0d1c36cebbace259b66a31c427ae9b44dd4ac592
Parshant <parsh3456@gmail.com>
2026-07-13 15:38:09 +0530

    Add some practice of js

---
+++ 09_js/01_test.js
@@ -0,0 +1,12 @@
+// let cities = ["a", "b", "c"]
+// cities[5] = "d"
+// cities[4] = "fill"
+// console.log(cities)
+
+let city1 = ["a", "b"]
+let city2 = ["c", "d"]
+let city3 = [...city1, ...city2]
+
+console.log(city1)
+console.log(city2)
+console.log(city3)
