072603dd0d070890b1015dafe86be3d1be90fce9
Parshant <parsh3456@gmail.com>
2026-07-20 13:17:52 +0530

    add getting started app.jsx

---
+++ 13_react/01_gettingStarted/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+      <h3>Hello</h3>
+      <h4>Hello</h4>
+      <h5>Hello</h5>
+      <h6>Hello</h6>
+    </>
+  )
+}
+
+export default App
