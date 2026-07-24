6ba20045e05d595524a0f9d0d1904dec28118fba
Parshant <parsh3456@gmail.com>
2026-07-24 13:34:11 +0530

    add ComingSoon Page

---
+++ 14_DevConnect/src/Pages/ComingSoon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComingSoon() {
+  return (
+    <div className="text-4xl font-bold flex w-full justify-center py-10">
+      <h1>Coming Soon</h1>
+    </div>
+  );
+}
+
+export default ComingSoon;
