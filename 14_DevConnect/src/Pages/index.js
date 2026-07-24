67631cece76b7e56056aa4cc39b1d1603f3c20b7
Parshant <parsh3456@gmail.com>
2026-07-24 13:35:39 +0530

    add Index file for Pages

---
+++ 14_DevConnect/src/Pages/index.js
@@ -0,0 +1,10 @@
+import Home from './Home'
+import About from './About'
+import Features from './Features'
+import Login from './Login'
+import SignUp from './SignUp'
+import ComingSoon from './ComingSoon'
+import Projects from './Projects'
+import Settings from './Settings'
+
+export {Home, About, Features, Login, SignUp, ComingSoon, Projects, Settings}
