171cfbb6cdbc16289d94b23fcd8d1a494e4c384b
Parshant <parsh3456@gmail.com>
2026-07-27 00:55:42 +0530

    add Layout for DashBoard Pages(after successful authorization)

---
+++ 14_DevConnect/src/Layout/DashboardLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+
+function DashboardLayout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header type="authNav" />
+
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default DashboardLayout;
