d36a814d230233998a009cc1bf5d8a6f235f29d8
Parshant <parsh3456@gmail.com>
2026-07-26 19:02:26 +0530

    add About Github component

---
+++ 14_DevConnect/src/components/About/AboutGithub.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function AboutGithub() {
+  return (
+    <div className="p-4">
+      <div className="w-full bg-purple-100 border border-blue-400/30 items-center flex justify-between p-4 rounded-lg shadow-lg gap-8">
+        <div className="flex items-center gap-4 w-1/3">
+          <div className="p-2 bg-blue-300 rounded-full w-fit h-fit">
+            <img
+              src="https://cdn-icons-png.flaticon.com/256/6517/6517429.png"
+              alt=""
+              width="70px"
+              className="bg-blue-700 p-1 rounded-full"
+            />
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">Built with ❤️ by Developers</h2>
+            <h4 className="text-[14px]">
+              DevConnect is open source and community-driven. Together, we can
+              build somethinh amazing.
+            </h4>
+          </div>
+        </div>
+        <a
+          href="https://github.com/Parshant124"
+          target="_blank"
+          className="flex items-center h-fit gap-4 border-purple-700/30 border-2 rounded px-4 py-2"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/270/270798.png"
+            alt=""
+            width="24px"
+          />
+          <h4 className="font-bold text-[14px] text-purple-700">
+            View on GitHub
+          </h4>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default AboutGithub
