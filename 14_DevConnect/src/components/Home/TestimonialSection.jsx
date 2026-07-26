8eeb6f6b91cfe1f907da2f2e838473e492d83840
Parshant <parsh3456@gmail.com>
2026-07-27 00:51:42 +0530

    add Testimonial Section of Home Page

---
+++ 14_DevConnect/src/components/Home/TestimonialSection.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function TestimonialSection() {
+  return (
+    <div className="flex w-full pt-8 justify-center">
+      <div className="flex gap-4 w-120 shadow-md border border-black/20 rounded-lg p-5">
+        <div className="bg-gray-400 w-1/5 h-fit rounded-full pt-0.5">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/265/265674.png"
+            alt=""
+            width="60px"
+            className="object-cover overflow-hidden rounded-full"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <div>
+            <img src=".\src\assets\5 stars.png" alt="" width="100px" />
+          </div>
+          <div className="text-gray-600 text-[14px]">
+            "DevConnect has completely transformed the way I manage my projects.
+            Highly recommended!"
+          </div>
+          <div className="text-gray-600 text-[12px]">
+            - Alex Johnson, Full Stack Developer
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestimonialSection
