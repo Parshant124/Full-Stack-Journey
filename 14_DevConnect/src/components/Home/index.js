80c01f550a90404583b5a4e0d096753fc59059a3
Parshant <parsh3456@gmail.com>
2026-07-27 00:52:13 +0530

    add index file of Home components

---
+++ 14_DevConnect/src/components/Home/index.js
@@ -0,0 +1,6 @@
+import CTASection from "./CTASection";
+import FeatureSection from "./FeatureSection";
+import Hero from "./Hero";
+import TestimonialSection from "./TestimonialSection";
+
+export {CTASection, FeatureSection, Hero, TestimonialSection}
