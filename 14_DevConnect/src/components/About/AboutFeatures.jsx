dd082b1d50938e8f2c533385b4715a9de0e44894
Parshant <parsh3456@gmail.com>
2026-07-26 19:02:02 +0530

    add About Features files

---
+++ 14_DevConnect/src/components/About/AboutFeatures.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import FeaturesCard from './FeaturesCard'
+
+function AboutFeatures() {
+  return (
+    <div className="flex gap-4 w-full justify-around py-8 px-4">
+      <div className="w-1/5">
+        <FeaturesCard
+          iconbg="blue-200"
+          icon="https://cdn-icons-png.flaticon.com/128/7829/7829198.png"
+          title="Connect"
+          desc="Build meaningfull connections with developers worldwide."
+        />
+      </div>
+      <div className="w-1/5">
+        <FeaturesCard
+          iconbg="green-200"
+          icon="https://cdn-icons-png.flaticon.com/128/3573/3573187.png"
+          title="Collaborate"
+          desc="Work together on innovative projects and solve problems."
+        />
+      </div>
+      <div className="w-1/5">
+        <FeaturesCard
+          iconbg="purple-200"
+          icon="https://cdn-icons-png.flaticon.com/128/18058/18058194.png"
+          title="Learn"
+          desc="Share knowledge and learn from the community."
+        />
+      </div>
+      <div className="w-1/5">
+        <FeaturesCard
+          iconbg="orange-200"
+          icon="https://cdn-icons-png.flaticon.com/128/2285/2285559.png"
+          title="Grow"
+          desc="Advance your skills and accelerate your career."
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AboutFeatures
---
+++ 14_DevConnect/src/components/About/FeaturesCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function FeaturesCard({iconbg, icon, title, desc }) {
+  return (
+    <div className="text-center flex flex-col items-center gap-2 border border-black/30 rounded-lg p-4 shadow-xl">
+      <div className={`p-2 bg-${iconbg} w-fit rounded-full`}>
+        <img 
+        src={icon} 
+        alt="" 
+        width="35px"
+        />
+      </div>
+      <h2 className="font-bold text-[18px]">{title}</h2>
+      <h4 className="text-[14px]">{desc}</h4>
+    </div>
+  );
+}
+
+export default FeaturesCard;
