075f7aa3b8472460b652d6c05842bda83e540c8e
Parshant <parsh3456@gmail.com>
2026-07-26 19:04:02 +0530

    add About Mission Component

---
+++ 14_DevConnect/src/components/About/AboutOurMission.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ABoutOurMission() {
+  return (
+    <div className="flex justify-center w-full">
+      <div className='w-1/3 text-center flex flex-col gap-2'>
+        <h2 className='font-bold text-2xl'>Our Mission</h2>
+        <h4 className='text-[14px]'>
+          To empower developers by providing a platform to showcase their work,
+          connect with peers and grow together
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+export default ABoutOurMission
