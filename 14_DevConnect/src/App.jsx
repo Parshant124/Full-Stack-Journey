9bc3ced6d6981d5ae4ab2b8f391b1fb2db31b567
Parshant <parsh3456@gmail.com>
2026-07-26 19:00:03 +0530

    add modified App.jsx

--- 14_DevConnect/src/App.jsx
+++ 14_DevConnect/src/App.jsx
@@ -1,28 +1,62 @@
-import { useState } from 'react'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
-import {About, ComingSoon, Features, Home, Login, SignUp} from './Pages/index'
-import Layout from './Layout'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />}/>
-      <Route path="features" element={<Features />}/>
-      <Route path='about' element={<About />}/>
-      <Route path='login' element={<Login />}/>
-      <Route path='signup' element={<SignUp />}/>
-      <Route path='comingsoon' element={<ComingSoon />}/>
-    </Route>
-  )
-)
+import { useState } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import {
+  About,
+  ComingSoon,
+  Features,
+  Home,
+  Login,
+  SignUp,
+  Profile,
+  Projects,
+  Settings,
+  DashBoard,
+} from "./Pages/index";
+import { useLoggedIn } from "./contexts";
+import MarketingLayout from "./Layout/MarketingLayout";
+import AuthLayout from "./Layout/AuthLayout";
+import DashboardLayout from "./Layout/DashboardLayout";
 
 function App() {
+  const { isLoggedIn, setIsLoggedIn } = useLoggedIn();
+
+  setIsLoggedIn(false);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route element={<MarketingLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="features" element={<Features />} />
+        </Route>
+        ,
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+        </Route>
+        , //{" "}
+        {/* <Route element={<DashboardLayout />}>
+          // <Route path="dashboard" element={<Dashboard />} />
+          // <Route path="projects" element={<Projects />} />
+          //{" "}
+        </Route> */}
+        ,
+      </>,
+    ),
+  );
 
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
