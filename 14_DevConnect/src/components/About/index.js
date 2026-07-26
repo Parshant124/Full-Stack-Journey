e0ce6337e62f28741b35b50d19c2fd06587f4fd9
Parshant <parsh3456@gmail.com>
2026-07-26 19:04:24 +0530

    add other about files

---
+++ 14_DevConnect/src/components/About/index.js
@@ -0,0 +1,7 @@
+import AboutDetails from "./AboutDetails";
+import AboutFeatures from "./AboutFeatures";
+import AboutStats from "./AboutStats";
+import AboutGithub from "./AboutGithub";
+import AboutOurMission from "./AboutOurMission";
+
+export {AboutDetails, AboutFeatures, AboutGithub, AboutStats, AboutOurMission}
---
+++ 14_DevConnect/src/components/Extra.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Extra() {
+  return (
+    <>
+    <h1 className='text-blue-600 bg-blue-200'></h1>
+    <h1 className='text-purple-600 bg-purple-200'></h1>
+    <h1 className='text-green-600 bg-green-200'></h1>
+    <h1 className='text-orange-600 bg-orange-200'></h1>
+    </>
+  )
+}
+
+export default Extra
