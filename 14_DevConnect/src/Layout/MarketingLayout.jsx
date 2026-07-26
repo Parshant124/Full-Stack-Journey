b9ebcbb5c049a8972c325387e95f1d6759bdd283
Parshant <parsh3456@gmail.com>
2026-07-27 00:55:07 +0530

    add Layout for Marketing Pages

---
+++ 14_DevConnect/src/Layout/MarketingLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function MarketingLayout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header type="marketingNav" />
+
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default MarketingLayout;
