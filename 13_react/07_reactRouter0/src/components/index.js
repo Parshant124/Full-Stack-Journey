a70f312ac16bcee47a7c2de362216df7fb56a34b
Parshant <parsh3456@gmail.com>
2026-07-29 15:01:45 +0530

    add index file for components

---
+++ 13_react/07_reactRouter0/src/components/index.js
@@ -0,0 +1,7 @@
+import Contact from './Contact.jsx'
+import About from './About.jsx'
+import Footer from './Footer.jsx'
+import Header from './Header.jsx'
+import Home from './Home.jsx'
+
+export {Contact, About, Footer, Header, Home}
