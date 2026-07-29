0d0c24f6681576dd360a51ffd96d73f897c4d857
Parshant <parsh3456@gmail.com>
2026-07-29 23:52:20 +0530

    add the DashBoard Pages

---
+++ 14_DevConnect/src/Pages/DashBoardPages/My_Projects.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function My_Projects() {
+  return (
+    <div>My_Projects</div>
+  )
+}
+
+export default My_Projects
---
+++ 14_DevConnect/src/Pages/DashBoardPages/index.js
@@ -0,0 +1,12 @@
+import Bookmarks from "./Bookmarks";
+import Connections from "./Connections";
+import DashBoard from "./DashBoard";
+import Explore from "./Explore";
+import My_Projects from "./My_Projects";
+import Notifications from "./Notifications";
+import Profile from "./Profile";
+import Projects from "./Projects";
+import Settings from "./Settings";
+import Tasks from "./Tasks";
+
+export {Bookmarks, Connections, DashBoard, Explore, My_Projects, Notifications, Profile, Projects, Settings, Tasks}
