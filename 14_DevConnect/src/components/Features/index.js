e8dd19641df83ad17299d495d2c43019ffa9b973
Parshant <parsh3456@gmail.com>
2026-07-29 15:06:00 +0530

    add index file for the components of Feature Page

---
+++ 14_DevConnect/src/components/Features/index.js
@@ -0,0 +1,5 @@
+import FeatureDisplay from "./FeatureDisplay";
+import FeatureJoin from "./FeatureJoin";
+import FeatureTitle from "./FeatureTitle";
+
+export {FeatureDisplay, FeatureJoin, FeatureTitle}
