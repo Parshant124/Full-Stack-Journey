ac1373700cee915e6437877d1e2a4916a3bdbcc5
Parshant <parsh3456@gmail.com>
2026-07-31 01:42:43 +0530

    added sidebar and outlet in the layout

--- 14_DevConnect/src/Layout/DashboardLayout.jsx
+++ 14_DevConnect/src/Layout/DashboardLayout.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
+import SideBar from "../components/SideBar";
 
 function DashboardLayout() {
+  const location = useLocation()
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="h-screen flex flex-col">
       <Header type="dashNav" />
 
-      <main className="flex-1">
+      <main
+        className={`flex-1 ${location.pathname === "/profile" ? "block" : "hidden"}`}
+      >
         <Outlet />
       </main>
+      <div
+        className={`flex w-full h-full flex-1 ${location.pathname !== "/profile" ? "block" : "hidden"} overflow-hidden`}
+      >
+        <aside className="w-1/6 overflow-y-auto">
+          <SideBar />
+        </aside>
+        <main className="w-5/6 flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
