a1004002bb7360539e3f610d94c0961a88892cca
Parshant <parsh3456@gmail.com>
2026-07-31 01:43:12 +0530

    added routing to the profile when clicked on the profile icon

--- 14_DevConnect/src/components/Nav/DashboardNav.jsx
+++ 14_DevConnect/src/components/Nav/DashboardNav.jsx
@@ -1,28 +1,48 @@
 import React from 'react'
 import { NavLink, Link } from "react-router-dom";
-import { useCurrUser } from '../../contexts';
+import { useCurrSessionUser } from '../../contexts';
 
 function DashboardNav() {
-  const {currUserId} = useCurrUser()
+  const {currSessionUserId} = useCurrSessionUser()
   return (
     <div className="flex w-full justify-between">
       <div className="w-3/5 flex justify-between items-center font-medium">
-        <NavLink to="/dashboard" className={`cursor-pointer`}>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            `${isActive ? "text-purple-800 underline underline-offset-8" : "text-gray-700"} cursor-pointer`
+          }
+        >
           Dashboard
         </NavLink>
-        <NavLink to="/projects" className={`cursor-pointer`}>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            `${isActive ? "text-purple-800 underline underline-offset-8" : "text-gray-700"} cursor-pointer`
+          }
+        >
           Projects
         </NavLink>
-        <NavLink to="/explore" className={`cursor-pointer`}>
+        <NavLink
+          to="/explore"
+          className={({ isActive }) =>
+            `${isActive ? "text-purple-800 underline underline-offset-8" : "text-gray-700"} cursor-pointer`
+          }
+        >
           Explore
         </NavLink>
-        <NavLink to="/notifications" className={`cursor-pointer`}>
+        <NavLink
+          to="/notifications"
+          className={({ isActive }) =>
+            `${isActive ? "text-purple-800 underline underline-offset-8" : "text-gray-700"} cursor-pointer`
+          }
+        >
           Notifications
         </NavLink>
       </div>
-      <NavLink className="">
+      <NavLink to="/profile" className="">
         <div className="w-8 h-8 bg-red-500 rounded-full flex justify-center items-center">
-          <h3 className="text-white">{currUserId[0] || "U"}</h3>
+          <h3 className="text-white">{currSessionUserId ? currSessionUserId[0].toUpperCase() : "U"}</h3>
         </div>
       </NavLink>
     </div>
