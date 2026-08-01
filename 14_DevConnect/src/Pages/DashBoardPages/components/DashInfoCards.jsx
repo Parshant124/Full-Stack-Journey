64ec0ca346199191406186b61539bb914178684f
Parshant <parsh3456@gmail.com>
2026-08-01 11:49:13 +0530

    link each card to the corresponding page

--- 14_DevConnect/src/Pages/DashBoardPages/components/DashInfoCards.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/components/DashInfoCards.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 function DashInfoCards({image, title, data, bgColor, linkTo}) {
   return (
-    <NavLink to={linkTo} className='flex items-center p-4 gap-4 rounded-lg shadow-md'>
+    <NavLink to={linkTo} className='flex items-center p-4 gap-4 rounded-lg shadow-md bg-white'>
         <div className={`bg-${bgColor} p-1 rounded-full`}><img src={image} alt="" width="50px" /></div>
         <div className='flex flex-col gap-2'>
             <h4 className='text-gray-600'>{title}</h4>
