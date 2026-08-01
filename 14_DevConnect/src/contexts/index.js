581359993e6f981790336f9a8ba6eaa4356efc3d
Parshant <parsh3456@gmail.com>
2026-08-01 11:47:00 +0530

    add Projects context to the index

--- 14_DevConnect/src/contexts/index.js
+++ 14_DevConnect/src/contexts/index.js
@@ -1,7 +1,9 @@
 import { AuthProvider, useAuth } from "./Authorized";
 import { CurrUserProvider, useCurrUser } from "./CurrUser";
 import { CurrSessionUserProvider, useCurrSessionUser} from "./CurrSessionUser"
+import { ProjectProvider, useProject } from "./Projects";
 
 export {AuthProvider, useAuth}
 export { CurrUserProvider, useCurrUser };
 export {CurrSessionUserProvider, useCurrSessionUser};
+export { ProjectProvider, useProject };
