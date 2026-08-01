20335520314d2b7ab9f302903e70e7073362c93f
Parshant <parsh3456@gmail.com>
2026-08-01 11:49:29 +0530

    modify some css on the components

--- 14_DevConnect/src/Pages/DashBoardPages/DashBoard.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/DashBoard.jsx
@@ -8,12 +8,12 @@ function DashBoard() {
   const {currSessionUserFullName} = useCurrSessionUser()
   const firstWord = currSessionUserFullName.substring(0, currSessionUserFullName.indexOf(" ") === -1 ? currSessionUserFullName.length : currSessionUserFullName.indexOf(" "));
   return (
-    <div className="overflow-y-auto h-full p-4 gap-6 flex flex-col">
+    <div className="overflow-y-auto h-full p-4 gap-6 flex flex-col bg-gray-50">
       <div
         style={{
           backgroundImage: `url(https://images.pexels.com/photos/7135053/pexels-photo-7135053.jpeg)`,
         }}
-        className="w-full h-40 flex items-center p-4 justify-between rounded-lg"
+        className="w-full h-40 flex items-center p-4 justify-between rounded-lg shadow-lg"
       >
         <div>
           <h2 className="font-bold text-2xl">Welcome back,{firstWord}</h2>
@@ -60,11 +60,11 @@ function DashBoard() {
         />
       </div>
       <div className="w-full h-full flex gap-4">
-        <div className="w-3/5 shadow-md h-full rounded-lg">
+        <div className="w-3/5 shadow-md h-full rounded-lg bg-white">
           {" "}
           <DashGraph />{" "}
         </div>
-        <div className="w-2/5 shadow-md h-full rounded-lg">
+        <div className="w-2/5 shadow-md h-full rounded-lg bg-white">
           {" "}
           <DashTasks />{" "}
         </div>
