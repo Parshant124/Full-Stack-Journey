ba5f0d711d27c63e86301aea244445d4fd65aa50
Parshant <parsh3456@gmail.com>
2026-08-01 11:46:21 +0530

    Link addproject route to the button

--- 14_DevConnect/src/Pages/DashBoardPages/My_Projects.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/My_Projects.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {NavLink} from 'react-router-dom'
 
 function My_Projects() {
+  const[addingProject, setAddingProject] = useState(false)
   return (
-    <div>My_Projects</div>
-  )
+    <div className="p-6">
+      <div className='flex justify-between items-center'>
+        <div>
+          <h2 className="text-2xl font-bold">My Projects</h2>
+          <h4 className='text-[14px] text-gray-600'>Manage and track all your projects.</h4>
+        </div>
+        <NavLink to="/addproject">
+          <button className={`bg-purple-700 px-2 py-1 text-white rounded-lg ${addingProject ? "hidden" : "flex"}`}
+            onClick={() => setAddingProject(true)}
+          >+ New Project</button>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default My_Projects
