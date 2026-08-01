b25010eb21e3a4da1c17427e5305b3fb4022e2ed
Parshant <parsh3456@gmail.com>
2026-08-01 11:47:45 +0530

    provide context to the Dash Layout

--- 14_DevConnect/src/Layout/DashboardLayout.jsx
+++ 14_DevConnect/src/Layout/DashboardLayout.jsx
@@ -1,30 +1,39 @@
-import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import React, {useState} from "react";
+import { Outlet, useLocation, useSearchParams } from "react-router-dom";
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
+import { ProjectProvider } from "../contexts";
 
 function DashboardLayout() {
+  const [projects, setProjects] = useState([]);
   const location = useLocation()
-  return (
-    <div className="h-screen flex flex-col">
-      <Header type="dashNav" />
 
-      <main
-        className={`flex-1 ${location.pathname === "/profile" ? "block" : "hidden"}`}
-      >
-        <Outlet />
-      </main>
-      <div
-        className={`flex w-full h-full flex-1 ${location.pathname !== "/profile" ? "block" : "hidden"} overflow-hidden`}
-      >
-        <aside className="w-1/6 overflow-y-auto">
-          <SideBar />
-        </aside>
-        <main className="w-5/6 flex-1 overflow-y-auto">
+  const addProject = () => {
+    // 
+  }
+
+  return (
+    <ProjectProvider value={{projects, addProject}}>
+      <div className="h-screen flex flex-col">
+        <Header type="dashNav" />
+
+        <main
+          className={`flex-1 ${location.pathname === "/profile" ? "block" : "hidden"}`}
+        >
           <Outlet />
         </main>
+        <div
+          className={`flex w-full h-full flex-1 ${location.pathname !== "/profile" ? "block" : "hidden"} overflow-hidden`}
+        >
+          <aside className="w-1/6 overflow-y-auto">
+            <SideBar />
+          </aside>
+          <main className="w-5/6 flex-1 overflow-y-auto">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </ProjectProvider>
   );
 }
 
