5a0cded479bf541f1fd7aa15fe3340780a13511e
Parshant <parsh3456@gmail.com>
2026-08-06 13:46:31 +0530

    add connection context

---
+++ 14_DevConnect/src/contexts/Connections.jsx
@@ -0,0 +1,16 @@
+import React, {createContext, useContext} from "react";
+
+const Connections = createContext({
+    connections: [],
+    pendingRequest: [],
+    addRequest: () => {},
+    deleteRequest: () => {},
+    addConnection: () => {},
+    deleteConnection: () => {}
+})
+
+export const ConnectionProvider = Connections.Provider
+
+export const useConnection = () => {
+    return (useContext(Connections));
+}
--- 14_DevConnect/src/contexts/index.js
+++ 14_DevConnect/src/contexts/index.js
@@ -3,9 +3,11 @@ import { CurrUserProvider, useCurrUser } from "./CurrUser";
 import { CurrSessionUserProvider, useCurrSessionUser} from "./CurrSessionUser"
 import { ProjectProvider, useProject } from "./Projects";
 import { TasksProvider, useTasks } from "./Tasks";
+import { ConnectionProvider, useConnection } from "./Connections";
 
 export {AuthProvider, useAuth}
 export { CurrUserProvider, useCurrUser };
 export {CurrSessionUserProvider, useCurrSessionUser};
 export { ProjectProvider, useProject };
 export { TasksProvider, useTasks };
+export { ConnectionProvider, useConnection };
