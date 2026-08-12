c7b5334274f5df59a77b873e33de259005d52466
Parshant <parsh3456@gmail.com>
2026-08-12 14:52:44 +0530

    add feature to search the project

--- 14_DevConnect/src/Pages/DashBoardPages/Bookmarks.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/Bookmarks.jsx
@@ -1,18 +1,39 @@
-import React,{useState} from "react";
-import {useBookMark, useCurrSessionUser, useCurrUser} from "../../contexts"
+import React, { useState } from "react";
+import {
+  useBookMark,
+  useCurrSessionUser,
+  useCurrUser,
+  useProject,
+} from "../../contexts";
 import BookMarkCard from "./components/BookMarkCard";
 
 function Bookmarks() {
-  const[searchValue, setSearchValue] = useState("")
-  const {bookmarks, removeBookMark} = useBookMark()
-  const{currUserId} = useCurrUser();
-  const{currSessionUserId} = useCurrSessionUser();
+  const [searchValue, setSearchValue] = useState("");
+  const [searchProject, setSearchProject] = useState([]);
+  const { bookmarks, removeBookMark } = useBookMark();
+  const { currUserId } = useCurrUser();
+  const { currSessionUserId } = useCurrSessionUser();
+  const { projects } = useProject();
 
   const userId = currSessionUserId || currUserId;
 
-  const myBookMarks = bookmarks.filter((curr) => curr.user === userId)
+  const myBookMarks = bookmarks.filter((curr) => curr.user === userId);
 
-  
+  const searchBookMark = (str) => {
+    setSearchValue(str);
+    if (!str.trim()) return;
+
+    setSearchProject(
+      myBookMarks.filter((bookmark) => {
+        const currProject = projects.find(
+          (project) => project.createdOn === bookmark.project,
+        );
+
+        return currProject?.name.toLowerCase().includes(str.toLowerCase());
+      }),
+    );
+  };
+
   return (
     <div className="p-4 flex flex-col gap-4 bg-gray-100 h-full">
       <div>
@@ -27,10 +48,12 @@ function Bookmarks() {
           className="border-2 px-2 py-1 text-[14px] rounded-md bg-white border-gray-300 w-1/2"
           placeholder="Search bookmarks..."
           value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
+          onChange={(e) => searchBookMark(e.target.value)}
         />
       </div>
-      <div className="bg-white p-6 rounded-lg">
+      <div
+        className={`${searchValue ? "hidden" : "block"} bg-white p-6 rounded-lg`}
+      >
         {myBookMarks.length > 0 ? (
           myBookMarks.map((bookmark) => (
             <BookMarkCard projectId={bookmark.project} />
@@ -38,6 +61,21 @@ function Bookmarks() {
         ) : (
           <div className="flex items-center justify-center">
             <h4 className="text-3xl font-bold text-gray-500">Nothing...</h4>
+          </div>
+        )}
+      </div>
+      <div
+        className={`${searchValue ? "block" : "hidden"} bg-white p-6 rounded-lg`}
+      >
+        {searchProject.length > 0 ? (
+          searchProject.map((bookmark) => (
+            <BookMarkCard projectId={bookmark.project} />
+          ))
+        ) : (
+          <div className="flex items-center justify-center">
+            <h4 className="text-3xl font-bold text-gray-500">
+              Nothing here...
+            </h4>
           </div>
         )}
       </div>
