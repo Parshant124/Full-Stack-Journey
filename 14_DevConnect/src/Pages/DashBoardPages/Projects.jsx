aad77504cb00c4f8af08e8423862876709f06910
Parshant <parsh3456@gmail.com>
2026-08-19 14:17:00 +0530

    add bookmark NavLink

--- 14_DevConnect/src/Pages/DashBoardPages/Projects.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/Projects.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useAuth, useBookMark, useCurrSessionUser, useCurrUser, useProject } from "../../contexts";
 import SeachProject from "./components/SeachProject";
 import ShowProject from "./components/ShowProject";
+import { NavLink } from "react-router-dom";
 
 function Projects() {
   const [searchValue, setSearchValue] = useState("")
@@ -31,11 +32,16 @@ function Projects() {
 
   return (
     <div className="p-6 flex flex-col gap-4 bg-gray-100 h-fit min-h-full">
-      <div>
+      <div className="flex justify-between">
+        <div>
         <h2 className="font-bold text-2xl">Projects</h2>
         <h4 className="text-[14px] text-gray-600">
           Discover amazing projects from the community.
         </h4>
+      </div>
+      <div>
+        <NavLink to="/bookmarks" className="text-purple-600 text-[14px] hover:underline underline-offset-2">all BookMarks</NavLink>
+      </div>
       </div>
       <div>
         <input
