c098056cdacafbe2887e479542a46badb0623a60
Parshant <parsh3456@gmail.com>
2026-08-23 17:18:33 +0530

    add Tasks page files to display user's tasks

--- 14_DevConnect/src/Pages/DashBoardPages/UserTask.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/UserTask.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useAuth, useTasks } from "../../contexts";
+import UserTaskCard from "./components/UserTaskCard";
 
 function UserTask() {
   const { userName } = useParams();
+  const { tasks } = useTasks();
+  const { Users } = useAuth();
 
-  return <div>UserTask : {userName}</div>;
+  const userTasks = tasks.filter((task) => task.userId === userName);
+  const userInfo = Users.find((user) => user.id === userName);
+
+  return (
+    <div>
+      <div className="p-4 flex justify-between">
+        <div>
+          <h2 className="text-2xl font-bold">
+            <span className="text-purple-600">{userInfo.fullName}'s</span> Tasks
+          </h2>
+          <h4 className="text-[14px] text-gray-600">
+            These are the tasks assigned to {userInfo.fullName}
+          </h4>
+        </div>
+        <div>
+          <div className="flex items-center gap-2">
+            <div className="w-4 h-4 bg-green-300 rounded-sm"></div>
+            <h4 className="text-[14px]">Completed</h4>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="w-4 h-4 bg-red-300 rounded-sm"></div>
+            <h4 className="text-[14px]">In Progress</h4>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-50 h-full">
+        {userTasks.length > 0 ? (
+          userTasks.map((task) => (
+            <div className="p-2">
+              <UserTaskCard task={task} />
+            </div>
+          ))
+        ) : (
+          <div>"Nothing</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default UserTask;
