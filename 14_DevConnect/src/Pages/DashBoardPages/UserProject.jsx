d2ad9e4ffc71d7ab61d3e8743ff596de73ec4125
Parshant <parsh3456@gmail.com>
2026-08-23 15:07:17 +0530

    add user Project page files

--- 14_DevConnect/src/Pages/DashBoardPages/UserProject.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/UserProject.jsx
@@ -1,26 +1,47 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useBookMark, useProject } from "../../contexts";
+import { useAuth, useBookMark, useCurrSessionUser, useCurrUser, useProject } from "../../contexts";
 import UserProjectCard from "./components/UserProjectCard";
 
 function UserProject() {
   const { projects } = useProject();
   const { userName } = useParams();
   const { bookmarks } = useBookMark();
+  const {currSessionUserId} = useCurrSessionUser();
+  const {currUserId} = useCurrUser();
+  const {Users} = useAuth();
+
+  const userId = currSessionUserId || currUserId;
+  const userInfo = Users.find((user) => user.id === userName);
+
   const userProjects = projects.filter(
     (project) => project.userId === userName && project.visibility === "Public",
   );
-
+  const myBookMarks = bookmarks
+    .filter((bookmark) => bookmark.user === userId)
+    .map((bookmark) => bookmark.project);
 
   return (
-    <div>
-      {userProjects.length > 0
-        ? userProjects.map((project) => (
-            <div className="flex flex-row">
-                <UserProjectCard project={project}/>
-            </div>
-          ))
-        : "NOTHING"}
+    <div className="flex flex-col h-full">
+      <div className="bg-white p-4 shadow-lg">
+        <h2 className="text-2xl font-bold">
+          <span className="text-purple-600">{userInfo.fullName}'s</span> Projects
+        </h2>
+        <h4 className="text-[14px] text-gray-600">These are the projects previews created by the user.</h4>
+      </div>
+      <div className="bg-gray-50 w-full h-full flex">
+        {userProjects.length > 0
+          ? userProjects.map((project) => (
+              <div className="p-4">
+                <UserProjectCard
+                  project={project}
+                  bookmarked={myBookMarks.includes(project.createdOn)}
+                  userId={userId}
+                />
+              </div>
+            ))
+          : "NOTHING"}
+      </div>
     </div>
   );
 }
--- 14_DevConnect/src/Pages/DashBoardPages/components/UserProjectCard.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/components/UserProjectCard.jsx
@@ -1,18 +1,54 @@
 import React from "react";
+import { useBookMark } from "../../../contexts";
 
-function UserProjectCard({ project, bookmarked }) {
+function UserProjectCard({ project, bookmarked, userId }) {
+    const {addBookMark, removeBookMark} = useBookMark()
   return (
-    <div className="w-80 bg-red-700 p-2 flex flex-col">
+    <div className="w-80 bg-white p-2 rounded-md shadow-lg flex flex-col gap-2">
       <div className="h-55 flex w-full">
-        <img src={project.image} alt="" className="object-cover"/>
+        <img
+          src={
+            project.image ||
+            `https://images.pexels.com/photos/30547618/pexels-photo-30547618.jpeg`
+          }
+          alt=""
+          className="w-full h-full object-cover rounded-md shadow-md"
+        />
       </div>
-      <div>
+      <div className="flex justify-between items-center">
         <div>
-          <h2>{project.name}</h2>
-          <h4>{project.desc}</h4>
+          <h2 className="font-semibold text-lg line-clamp-1 overflow-hidden">
+            {project.name}
+          </h2>
+          <h4 className="text-[14px] text-gray-600 line-clamp-2 overflow-hidden">
+            {project.desc}
+          </h4>
+          <h4 className="text-[14px] text-purple-800 bg-purple-200 w-fit px-2 py-1 rounded-full">
+            {project.category}
+          </h4>
         </div>
         <div>
-            {bookmarked ? "Yes" : "No"}
+          {bookmarked ? (
+            <div
+              className="h-7 flex"
+              onClick={() => removeBookMark(userId, project.createdOn)}
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/2740/2740595.png"
+                alt=""
+              />
+            </div>
+          ) : (
+            <div
+              className="h-7 flex"
+              onClick={() => addBookMark(userId, project.createdOn)}
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/3106/3106777.png"
+                alt=""
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
