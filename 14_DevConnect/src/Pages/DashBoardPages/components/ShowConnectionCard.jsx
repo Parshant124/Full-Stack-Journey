f98bad68e8d0d0403311bd6a16589e8695eab244
Parshant <parsh3456@gmail.com>
2026-08-23 12:52:15 +0530

    link user to their profiles

--- 14_DevConnect/src/Pages/DashBoardPages/components/ShowConnectionCard.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/components/ShowConnectionCard.jsx
@@ -76,7 +76,7 @@ function ShowConnectionCard({ userId }) {
       className="flex w-full justify-between border-b-2 pb-4 border-gray-300"
       key={userId}
     >
-      <NavLink to={`/user/${userInfo.id}`} className="flex gap-4 items-center">
+      <NavLink to={`/profile/${userInfo.id}`} className="flex gap-4 items-center">
         <div className="h-12 w-12 flex">
           <img
             src={
