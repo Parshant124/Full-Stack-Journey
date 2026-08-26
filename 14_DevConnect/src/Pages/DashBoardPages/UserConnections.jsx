775a8a267eb60384183d07f9e47919069820f4ad
Parshant <parsh3456@gmail.com>
2026-08-26 22:34:49 +0530

    solve error to not contain the curr User in the followers list

--- 14_DevConnect/src/Pages/DashBoardPages/UserConnections.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/UserConnections.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useConnection } from "../../contexts";
+import { useConnection, useCurrSessionUser, useCurrUser } from "../../contexts";
 import UserConnectionCard from "./components/UserConnectionCard";
 
 function UserConnections() {
   const { userName } = useParams();
   const { connections } = useConnection();
+  const { currSessionUserId } = useCurrSessionUser();
+  const { currUserId } = useCurrUser();
+
+  const currId = currSessionUserId || currUserId;
+
   const userConnection = connections
     .filter(
       (connection) =>
-        connection.senderId === userName || connection.receiverId === userName,
+        (connection.senderId === userName ||
+          connection.receiverId === userName) &&
+        connection.senderId !== currId &&
+        connection.receiverId !== currId,
     )
     .map((connection) =>
       connection.senderId === userName
@@ -17,12 +25,12 @@ function UserConnections() {
         : connection.senderId,
     );
 
-  console.log(userConnection);
-
   return (
     <div>
       {userConnection.map((connection) => (
-        <div><UserConnectionCard userId={connection}/></div>
+        <div>
+          <UserConnectionCard userId={connection} />
+        </div>
       ))}
     </div>
   );
