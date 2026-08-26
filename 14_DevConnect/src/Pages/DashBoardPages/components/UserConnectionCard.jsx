ffcd270b4f902070c795bb569913ba9f835fae56
Parshant <parsh3456@gmail.com>
2026-08-26 22:35:43 +0530

    link user to their respective profiles

--- 14_DevConnect/src/Pages/DashBoardPages/components/UserConnectionCard.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/components/UserConnectionCard.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import { useAuth, useConnection, useCurrSessionUser, useCurrUser } from '../../../contexts'
+import { NavLink } from 'react-router-dom';
 
 function UserConnectionCard({userId}) {
       const [showMessage, setShowMessage] = useState(false);
@@ -70,12 +71,29 @@ function UserConnectionCard({userId}) {
       setConnectionStatus(2);
     };
   return (
-    <div className='relative'>
-      {userInfo.fullName}
-      <div className="border-b-2 pb-4 border-gray-300 flex justify-center pt-2">
+    <div className="relative flex border-b-2 p-4 border-gray-300 justify-between w-full">
+      <NavLink to={`/profile/${userId}`} className="flex items-center gap-2">
+        <div className="h-12 w-12 flex">
+          <img
+            src={
+              userInfo.image ||
+              `https://cdn-icons-png.flaticon.com/128/3001/3001758.png`
+            }
+            alt=""
+            className="w-full h-full rounded-full object-cover"
+          />
+        </div>
+        <div className="flex flex-col">
+          <h2 className="font-semibold">{userInfo.fullName}</h2>
+          <h4 className="text-[14px] text-gray-600">
+            {userInfo.bio || "Bio not available"}
+          </h4>
+        </div>
+      </NavLink>
+      <div className="flex">
         {connectionStatus === 1 && (
           <button
-            className="border-2 w-full py-2  text-green-800 rounded-lg font-semibold"
+            className="border-2 w-full py-1 px-2 text-green-800 rounded-lg font-semibold"
             onClick={handleRemoveConnection}
           >
             Connected
@@ -84,22 +102,22 @@ function UserConnectionCard({userId}) {
 
         {connectionStatus === 2 && (
           <button
-            className="border-2 w-full py-2  text-purple-600 rounded-lg font-semibold"
+            className="border-2 w-full py-1 px-2 text-purple-600 rounded-lg font-semibold"
             onClick={handleDeleteRequest}
           >
             Request Sent
           </button>
         )}
         {connectionStatus === 3 && (
-          <div className="w-1/2 flex justify-around">
+          <div className="flex justify-around gap-4">
             <button
-              className="w-1/3 border-2 text-green-800 px-4 py-2 rounded-md font-semibold"
+              className="w-fit border-2 text-green-800 px-2 py-1 rounded-md font-semibold"
               onClick={handleAcceptRequest}
             >
               Accept
             </button>
             <button
-              className="w-1/3 border-2 text-red-600 px-4 py-2 rounded-md font-semibold"
+              className="w-fit border-2 text-red-600 px-2 py-1 rounded-md font-semibold"
               onClick={handleRejectRequest}
             >
               Reject
@@ -108,7 +126,7 @@ function UserConnectionCard({userId}) {
         )}
         {connectionStatus === 4 && (
           <button
-            className="border-2 w-full py-2 bg-purple-600 text-white rounded-lg font-semibold"
+            className="border-2 w-full py-1 px-2 bg-purple-600 text-white rounded-lg font-semibold"
             onClick={handleConnect}
           >
             Connect
