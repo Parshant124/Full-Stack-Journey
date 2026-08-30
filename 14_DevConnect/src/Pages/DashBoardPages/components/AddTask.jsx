37f886c9e2112db37772bed8957371cf84609dac
Parshant <parsh3456@gmail.com>
2026-08-30 22:53:05 +0530

    improved date format

--- 14_DevConnect/src/Pages/DashBoardPages/components/AddTask.jsx
+++ 14_DevConnect/src/Pages/DashBoardPages/components/AddTask.jsx
@@ -47,8 +47,10 @@ function AddTask() {
     }
 
     const today = new Date();
-    const formattedDate = `${String(today.getDate()).padStart(2, "0")}/${String(today.getMonth() + 1).padStart(2, "0")}/${today.getFullYear()}`;
-
+    const formattedDate = `${today.getFullYear()}-${String(
+      today.getMonth() + 1,
+    ).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`;
+    
     const task = {
       userId: currentUser?.id,
       taskId: Date.now(),
