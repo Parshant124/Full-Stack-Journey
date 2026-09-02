b807766fb461582c90b34ef45220887ce96d7055
Parshant <parsh3456@gmail.com>
2026-09-02 16:47:22 +0530

    linked card images from supabase

--- 14_DevConnect/src/components/Features/FeatureDisplay.jsx
+++ 14_DevConnect/src/components/Features/FeatureDisplay.jsx
@@ -7,7 +7,7 @@ function FeatureDisplay() {
         <Cards
           iconBg="blue-200"
           icon="https://cdn-icons-png.flaticon.com/128/12075/12075377.png"
-          imgDisplay=".\src\components\Features\assets\Card1.png"
+          imgDisplay="https://uftsixsunvrpbwrmcrre.supabase.co/storage/v1/object/public/devconnect-images/logos/Card1.png"
           title="Project Showcase"
           desc="Showcase your Projects to the world with beautiful portfolios and detailed documentation."
         />
@@ -16,7 +16,7 @@ function FeatureDisplay() {
         <Cards
           iconBg="purple-200"
           icon="https://cdn-icons-png.flaticon.com/128/8215/8215621.png"
-          imgDisplay=".\src\components\Features\assets\Card2.png"
+          imgDisplay="https://uftsixsunvrpbwrmcrre.supabase.co/storage/v1/object/public/devconnect-images/logos/Card2.png"
           title="Team Collaboration"
           desc="Collaborate with your team in real-time with discussion, tasks and project management tools."
         />
@@ -25,7 +25,7 @@ function FeatureDisplay() {
         <Cards
           iconBg="green-200"
           icon="https://cdn-icons-png.flaticon.com/128/171/171322.png"
-          imgDisplay=".\src\components\Features\assets\Card3.png"
+          imgDisplay="https://uftsixsunvrpbwrmcrre.supabase.co/storage/v1/object/public/devconnect-images/logos/Card3.png"
           title="Knowledge Sharing"
           desc="Share knowledge through articles, tutorials, and guides. Learn from the community."
         />
@@ -34,7 +34,7 @@ function FeatureDisplay() {
         <Cards
           iconBg="orange-200"
           icon="https://cdn-icons-png.flaticon.com/128/3094/3094956.png"
-          imgDisplay=".\src\components\Features\assets\Card4.png"
+          imgDisplay="https://uftsixsunvrpbwrmcrre.supabase.co/storage/v1/object/public/devconnect-images/logos/Card4.png"
           title="Real-time Updates"
           desc="Stay updated with real-time notifications on project activities, mentions and collaboraions."
         />
@@ -43,7 +43,7 @@ function FeatureDisplay() {
         <Cards
           iconBg="blue-200"
           icon="https://cdn-icons-png.flaticon.com/128/954/954591.png"
-          imgDisplay=".\src\components\Features\assets\Card5.png"
+          imgDisplay="https://uftsixsunvrpbwrmcrre.supabase.co/storage/v1/object/public/devconnect-images/logos/Card5.png"
           title="Discover Developers"
           desc="Find and connect with talented developers based on skills, interests, and experience."
         />
@@ -52,7 +52,7 @@ function FeatureDisplay() {
         <Cards
           iconBg="purple-200"
           icon="https://cdn-icons-png.flaticon.com/128/2592/2592317.png"
-          imgDisplay=".\src\components\Features\assets\Card6.png"
+          imgDisplay="https://uftsixsunvrpbwrmcrre.supabase.co/storage/v1/object/public/devconnect-images/logos/Card6.png"
           title="Secure & Private"
           desc="Your date and projects are protected with top-notchc security and privacy controls."
         />
